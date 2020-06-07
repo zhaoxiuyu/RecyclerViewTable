5e0f449b3e96af4246a7c714ce54728e6ef1c31f
zxy_it@yeah.net <zxy_it@yeah.net>
2020-06-07 21:46:28 +0800

    修复多个手指同时触摸滑动 造成多个列表不同步的问题。

--- app/src/main/java/com/sendinfo/recyclerviewtable/no_nested/NoHorizontalScrollView.kt
+++ app/src/main/java/com/sendinfo/recyclerviewtable/no_nested/NoHorizontalScrollView.kt
@@ -1,8 +1,8 @@
 package com.sendinfo.recyclerviewtable.no_nested
 
 import android.content.Context
-import android.widget.HorizontalScrollView
 import android.util.AttributeSet
+import android.widget.HorizontalScrollView
 
 /**
  * 自定义 HorizontalScrollView，向外提供滑动监听功能
@@ -20,13 +20,18 @@ class NoHorizontalScrollView : HorizontalScrollView {
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
 
-    fun setScrollViewListener(scrollViewListener: ScrollViewListener) {
-        this.scrollViewListener = scrollViewListener
+
+    override fun onScreenStateChanged(screenState: Int) {
+        super.onScreenStateChanged(screenState)
     }
 
     override fun onScrollChanged(x: Int, y: Int, oldx: Int, oldy: Int) {
         super.onScrollChanged(x, y, oldx, oldy)
         scrollViewListener?.onScrollChanged(this, x, y, oldx, oldy)
+    }
+
+    fun setScrollViewListener(scrollViewListener: ScrollViewListener) {
+        this.scrollViewListener = scrollViewListener
     }
 
     interface ScrollViewListener {
--- app/src/main/java/com/sendinfo/recyclerviewtable/no_nested/NoNestedActivity.kt
+++ app/src/main/java/com/sendinfo/recyclerviewtable/no_nested/NoNestedActivity.kt
@@ -1,12 +1,14 @@
 package com.sendinfo.recyclerviewtable.no_nested
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.blankj.utilcode.util.LogUtils
 import com.sendinfo.recyclerviewtable.DataUtils
 import com.sendinfo.recyclerviewtable.R
 import com.sendinfo.recyclerviewtable.RoomInfo
@@ -16,6 +18,8 @@ import kotlinx.android.synthetic.main.activity_no_nested.*
  * 多个recyclerView设置滚动监听，垂直 水平 滚动同时带动其他recyclerView滚动
  */
 class NoNestedActivity : AppCompatActivity() {
+
+    private var isScroll = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +47,10 @@ class NoNestedActivity : AppCompatActivity() {
         rvContent.adapter = contentAdapter
         contentAdapter.setNewInstance(DataUtils.getDatas(RoomInfo.item_room_center, 224))
 
+        initLisenter()
+    }
+
+    private fun initLisenter() {
         // HorizontalScrollView 滑动就取消 两个列表的滑动监听
         nhsv.setScrollViewListener(object : NoHorizontalScrollView.ScrollViewListener {
             override fun onScrollChanged(
@@ -57,28 +65,64 @@ class NoNestedActivity : AppCompatActivity() {
             }
         })
 
+        var isAdd = false
         // 添加触摸事件
         rvLeft.addOnItemTouchListener(object : RecyclerView.OnItemTouchListener {
             private var mLastY: Int = 0
             override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+
+                // 当前Rv处于空闲状态
                 if (rv.scrollState == RecyclerView.SCROLL_STATE_IDLE) {
                     onTouchEvent(rv, e)
+                    return false
+                }
+                if (rvContent.scrollState != RecyclerView.SCROLL_STATE_IDLE) {
+                    onTouchEvent(rv, e)
+                    return true
                 }
                 return false
             }
 
             override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
-                // 若是手指按下的动作，且另一个列表处于空闲状态
-                if (e.action == MotionEvent.ACTION_DOWN && rvContent.scrollState == RecyclerView.SCROLL_STATE_IDLE) {
-                    // 记录当前另一个列表的y坐标并对当前列表设置滚动监听
-                    mLastY = rv.scrollY
-                    rv.addOnScrollListener(leftScroll)
-                } else {
-                    // 若当前列表原地抬起手指时，移除当前列表的滚动监听
-                    if (e.action == MotionEvent.ACTION_UP && rv.scrollY == mLastY) {
-                        rv.removeOnScrollListener(leftScroll)
+//                Log.d("NoNestedActivity", "onTouchEvent")
+
+                when (e.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        Log.d("NoNestedActivity", "左侧 按下")
+
+                        rvLeft.stopScroll()
+                        rvContent.stopScroll()
+
+                        rvLeft.removeOnScrollListener(leftScroll)
+                        rvContent.removeOnScrollListener(contentScroll)
+
+                        rvLeft.addOnScrollListener(leftScroll)
+                    }
+                    MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                        Log.d("NoNestedActivity", "左侧 抬起 或 取消")
+
+                        rvLeft.removeOnScrollListener(leftScroll)
                     }
                 }
+
+//                // 若是手指按下的动作，且中间列表处于空闲状态
+//                if (e.action == MotionEvent.ACTION_DOWN && rvContent.scrollState == RecyclerView.SCROLL_STATE_IDLE) {
+//                    // 记录当前列表的y坐标,对当前列表设置滚动监听
+//                    mLastY = rv.scrollY
+//                    // 左侧列表滑动的时候，移除中间列表滑动监听
+//                    rvContent.removeOnScrollListener(contentScroll)
+//                    // 注册左侧列表滑动监听
+//                    rv.addOnScrollListener(leftScroll)
+//                    Log.d("NoNestedActivity", "注册滑动监听,移除其他rv滑动监听")
+//                } else {
+//                    // 当前列表抬起手指时，移除当前列表的滚动监听
+//                    if (e.action == MotionEvent.ACTION_UP && rv.scrollY == mLastY) {
+//                        rv.removeOnScrollListener(leftScroll)
+//                        Log.d("NoNestedActivity", "移除监听")
+//                    } else {
+//                        Log.d("NoNestedActivity", "不作处理")
+//                    }
+//                }
             }
 
             override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
@@ -89,6 +133,7 @@ class NoNestedActivity : AppCompatActivity() {
             private var mLastY: Int = 0
 
             override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+                // 当前Rv处于空闲状态
                 if (rv.scrollState == RecyclerView.SCROLL_STATE_IDLE) {
                     onTouchEvent(rv, e)
                 }
@@ -96,14 +141,39 @@ class NoNestedActivity : AppCompatActivity() {
             }
 
             override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
-                if (e.action == MotionEvent.ACTION_DOWN && rvLeft.scrollState == RecyclerView.SCROLL_STATE_IDLE) {
-                    mLastY = rv.scrollY
-                    rv.addOnScrollListener(contentScroll)
-                } else {
-                    if (e.action == MotionEvent.ACTION_UP && rv.scrollY == mLastY) {
-                        rv.removeOnScrollListener(contentScroll)
+                when (e.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        Log.d("NoNestedActivity2", "中间 按下")
+
+                        rvLeft.stopScroll()
+                        rvContent.stopScroll()
+
+                        rvLeft.removeOnScrollListener(leftScroll)
+                        rvContent.removeOnScrollListener(contentScroll)
+
+                        rvContent.addOnScrollListener(contentScroll)
+                    }
+                    MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                        Log.d("NoNestedActivity2", "中间 抬起 或 取消")
+
+                        rvContent.removeOnScrollListener(contentScroll)
                     }
                 }
+
+//                // 若是手指按下的动作，且左侧列表处于空闲状态
+//                if (e.action == MotionEvent.ACTION_DOWN && rvLeft.scrollState == RecyclerView.SCROLL_STATE_IDLE) {
+//                    // 记录当前列表的y坐标,对当前列表设置滚动监听
+//                    mLastY = rv.scrollY
+//                    // 中间列表滑动的时候，移除左侧列表滑动监听
+//                    rvLeft.removeOnScrollListener(leftScroll)
+//                    // 注册中间列表滑动监听
+//                    rv.addOnScrollListener(contentScroll)
+//                } else {
+//                    // 当前列表抬起手指时，移除当前列表的滚动监听
+//                    if (e.action == MotionEvent.ACTION_UP && rv.scrollY == mLastY) {
+//                        rv.removeOnScrollListener(contentScroll)
+//                    }
+//                }
             }
 
             override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
@@ -115,6 +185,7 @@ class NoNestedActivity : AppCompatActivity() {
     private val leftScroll = object : RecyclerView.OnScrollListener() {
         override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
             super.onScrolled(recyclerView, dx, dy)
+            Log.d("NoNestedActivity", "左侧 滑动 onScrolled")
             // 左侧列表滑动时带动中间订单滑动
             rvContent.scrollBy(dx, dy)
         }
@@ -122,8 +193,9 @@ class NoNestedActivity : AppCompatActivity() {
         override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
             super.onScrollStateChanged(recyclerView, newState)
             // 空闲状态就取消当前的滑动监听
+            Log.d("NoNestedActivity", "左侧 ${RecyclerView.SCROLL_STATE_IDLE}")
             if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                recyclerView.removeOnScrollListener(this)
+//                recyclerView.removeOnScrollListener(this)
             }
         }
     }
@@ -133,14 +205,16 @@ class NoNestedActivity : AppCompatActivity() {
         override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
             super.onScrolled(recyclerView, dx, dy)
             // 中间内容列表滑动带动左侧列表滑动
+            Log.d("NoNestedActivity2", "中间 滑动 onScrolled")
             rvLeft.scrollBy(dx, dy)
         }
 
         override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
             super.onScrollStateChanged(recyclerView, newState)
             // 空闲状态就取消当前的滑动监听
+            Log.d("NoNestedActivity2", "中间 ${RecyclerView.SCROLL_STATE_IDLE}")
             if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                recyclerView.removeOnScrollListener(this)
+//                recyclerView.removeOnScrollListener(this)
             }
         }
     }
